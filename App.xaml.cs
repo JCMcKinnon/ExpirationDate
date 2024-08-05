6b9181e9e73c57d3bebb10e99506291e18ce884d
Jason <jason.c.mckinnon@gmail.com>
2024-08-05 15:42:20 -0700

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpirationDate
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
